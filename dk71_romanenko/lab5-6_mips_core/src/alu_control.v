e309eca4cdff6e77ee2a3a551ad1b8393d8be5d1
Slavaroanenko3424 <gangstar7778@gamil.com>
2020-01-18 15:41:26 +0200

    lab5-6

---
+++ dk71_romanenko/lab5-6_mips_core/src/alu_control.v
@@ -0,0 +1,27 @@
+
+`include "asm_table.v"
+
+module alu_control(op, func, control);
+
+input wire [5:0] op;
+input wire [5:0] func;
+
+output reg [4:0] control;
+
+integer temp;
+
+always @* begin
+	if(!op) temp = func;
+	else temp = op;
+	
+	casez(op)
+		`BEQ: temp = `SUB;
+		`BNE: temp = `SUB;
+		`LW: temp = `ADD;
+		`SW: temp = `ADD;
+		default: control = temp[4:0];
+	endcase
+	
+	control = temp[4:0];
+end
+endmodule
